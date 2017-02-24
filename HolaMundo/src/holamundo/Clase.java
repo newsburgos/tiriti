8904cd27b7cd3a67aab48a6a4ed56bd1fbb334a0
ALUMNO <ALUMNO@203p15-PC>
2017-02-24 09:44:28 +0100

    CAMBIO ACTIVIDAD UNO

---
+++ HolaMundo/src/holamundo/Clase.java
@@ -0,0 +1,22 @@
+package holamundo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ALUMNO
+ */
+public class Clase {
+    int a ;
+    public Clase(){
+        a=7;
+    }
+    public void modificacionActividadUno(){
+        
+    }
+    
+}
